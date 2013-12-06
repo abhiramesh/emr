e5cefac53f4a111a5ab9cd1e86074b81faf15e25
Abhi Ramesh <abhiramesh@gmail.com>
2013-12-05 21:23:16 -0500

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emr::Application.config.secret_token = 'f35aa8b52d565a99794b3ce2407834bc95d7b88c3a0583d597163fde2b9468ce91797e5b2d4b32053dd24a1b47b7fc91221ecb2c63ffca21abb65e2145163fbd'
